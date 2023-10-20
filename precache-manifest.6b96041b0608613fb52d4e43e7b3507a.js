1d6cc82710b1934c7cc44cd95054595bce88e59b
Alicia Soh <aliciasoh@Alicias-MacBook-Pro>
2023-10-20 13:09:11 +0800

    Updates

---
+++ precache-manifest.6b96041b0608613fb52d4e43e7b3507a.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e58e8c4b673c99815002dde598962a4b",
+    "url": "/website/index.html"
+  },
+  {
+    "revision": "9731f6f2d805c38c187a",
+    "url": "/website/static/css/main.691c0b82.chunk.css"
+  },
+  {
+    "revision": "c164d4dc1334d049b0ad",
+    "url": "/website/static/js/2.58382ee2.chunk.js"
+  },
+  {
+    "revision": "9731f6f2d805c38c187a",
+    "url": "/website/static/js/main.dcbd1466.chunk.js"
+  },
+  {
+    "revision": "f4f1f0c067694d506fa6",
+    "url": "/website/static/js/runtime~main.66790c67.js"
+  },
+  {
+    "revision": "f765f3fbde6ea6c049ec9f7789c904b4",
+    "url": "/website/static/media/advisory.f765f3fb.png"
+  },
+  {
+    "revision": "b453fd729aacf708a5d21e20a40a78fd",
+    "url": "/website/static/media/fb.b453fd72.jpg"
+  },
+  {
+    "revision": "aa9e32be8641ecd842cc990bab1bd90b",
+    "url": "/website/static/media/flask.aa9e32be.png"
+  },
+  {
+    "revision": "e282c96414ffdd63c5681b5aaf8aaed1",
+    "url": "/website/static/media/jtc.e282c964.png"
+  },
+  {
+    "revision": "df8fd3efcd662b64b44de07f351c838b",
+    "url": "/website/static/media/landing-bg.df8fd3ef.jpg"
+  },
+  {
+    "revision": "de5fd32c58aab09d66846f47be0c14c4",
+    "url": "/website/static/media/mino.de5fd32c.jpg"
+  },
+  {
+    "revision": "883d4f3c63b149cfa3009138a300f2e4",
+    "url": "/website/static/media/odafy.883d4f3c.png"
+  },
+  {
+    "revision": "8e2725c4da7cfbc852a5506ed4e2dd09",
+    "url": "/website/static/media/qi.8e2725c4.png"
+  },
+  {
+    "revision": "67170c6811e72aed87a4e665d4832253",
+    "url": "/website/static/media/sambla.67170c68.png"
+  },
+  {
+    "revision": "967df8a3cc30af47db1e899dc3788132",
+    "url": "/website/static/media/sc.967df8a3.png"
+  },
+  {
+    "revision": "6b3b513fadfc9e3b93779402e46c16f6",
+    "url": "/website/static/media/soccer.6b3b513f.jpeg"
+  },
+  {
+    "revision": "4aef49d68fde9e0d19240fa300b5812f",
+    "url": "/website/static/media/swedbank.4aef49d6.jpg"
+  },
+  {
+    "revision": "9be05746fb38ccb351b80b5cab2f4d9e",
+    "url": "/website/static/media/tech.9be05746.jpg"
+  },
+  {
+    "revision": "5f2fb1ce75bdd7bdc2875681936f7233",
+    "url": "/website/static/media/travel.5f2fb1ce.jpg"
+  },
+  {
+    "revision": "d5e89569bd13918f2544450529ab6959",
+    "url": "/website/static/media/wit.d5e89569.jpg"
+  }
+]);
